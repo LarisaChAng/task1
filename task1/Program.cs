f1afa858cd8b34435d960b376eae1213c69a96b3
LarisaChAng <LChS81@mail.ru>
2022-10-25 12:44:44 +0800

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[7];
+            int s = 0;
+            Console.WriteLine("Введите семь чисел одномерного массива");
+            for (int i = 0; i < 7; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                s += array[i];
+            }
+            Console.WriteLine("Среднеарифметическое чисел равно {0:f2}", ((double)s) / 7);
+            Console.ReadKey();
+        }
+    }
+}
